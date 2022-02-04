431a6f6d255ccfa854b462e8983908e301f35823
Sujin Jang <susan3722@snu.ac.kr>
2022-02-04 17:08:44 +0900

    풀이 2
    - 개선 : list가 변경되는 for문을 사용할 때 [:]를 사용해주면 빠지는 index 없이 처리됨

--- 체육복.py
+++ 체육복.py
@@ -4,42 +4,42 @@ def solution(n, lost, reserve):
     # reserve도, lost도 아닌 애들
     default = list(set(all) - set(lost) - set(reserve))
 
-    # 여벌이 있는 애가 lost 했다면 여벌에서 제외하기
-    for i in reserve :
-        if i in lost :
-            reserve[reserve.index(i)] = -i
-            lost[lost.index(i)] = -i
+    # 여벌이 있는 애가 lost 했다면 여벌, lost에서 제외하기
+    for i in reserve[:] :
+        if i in lost[:] :
+            reserve.remove(i)
+            lost.remove(i)
             default.append(i)
 
     # all = list(set(all) - set(default))
     reserve.sort()
     lost.sort()
 
-    for i in reserve :
-        if i != 1 and i > 0 :
+    for i in reserve[:] :
+        print(i)
+        if i != 1 :
             if i-1 in lost :
                 # i-1이 우선권을 가짐
-                lost[lost.index(i-1)] = -i+1
-                reserve[reserve.index(i)] = -i
+                lost.remove(i-1)
+                reserve.remove(i)
                 default.append(i)
                 default.append(i-1)
                 continue
             elif i+1 in lost :
-                lost[lost.index(i+1)] = -i-1
-                reserve[reserve.index(i)] = -i
+                lost.remove(i+1)
+                reserve.remove(i)
                 default.append(i)
                 default.append(i+1)
                 continue
         elif i == 1 :
             if 2 in lost :
-                lost[lost.index(2)] = -2
-                reserve[reserve.index(1)] = -1
+                lost.remove(2)
+                reserve.remove(1)
                 default.append(1)
                 default.append(2)
-            
+              
     # 여벌에 있는 양수 애들 default에 넣어주기
     for i in reserve :
-        if i > 0 :
-            default.append(i)
+        default.append(i)
     
-    return len(default)
+		return len(default)
