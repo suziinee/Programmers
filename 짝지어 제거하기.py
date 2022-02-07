e7e65da7d38cff3a03ad4b68f40f0e219bf79437
Sujin Jang <susan3722@snu.ac.kr>
2022-02-07 12:25:13 +0900

    풀이 1
    - stack의 마지막 원소와 같으면 append하지 않고 마지막 원소까지 pop
    - 아니면 append

---
+++ 짝지어 제거하기.py
@@ -0,0 +1,19 @@
+def solution(s):
+
+    s = list(s)
+    stack = [s[0]]
+    s.pop(0)
+
+    for word in s :
+        if stack : 
+            if stack[-1] == word :
+                stack.pop(-1)
+            else :
+                stack.append(word)
+        else :
+            stack.append(word)
+
+    if stack :
+        return 0
+    else :
+        return 1
