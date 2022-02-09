31f439cb8df80fe3c1ff51403c43e36c75675884
Sujin Jang <susan3722@snu.ac.kr>
2022-02-09 15:54:14 +0900

    풀이 2
    - for문 밖에 cnt를 append하는 if문을 넣음
    - for문을 돌면서 0보다 작거나 같으면 cnt += 1하고 pop, 0보다 크면 for문 빠져나가기
    - for문을 나온 후 cnt가 조건을 만족하면 answer에 append하고 cnt는 초기화
    - 이것도 테케 통과 못함

--- 기능개발.py
+++ 기능개발.py
@@ -1,19 +1,21 @@
 def solution(progresses, speeds) : 
     answer = []
+    cnt = 0
+    
+    progresses = [100-x for x in progresses]
 
     while progresses :
-        cnt = 0 
         progresses = [x - y for x, y in zip(progresses, speeds)]
         for i in progresses[:] :
-            if cnt == 0  and i > 0 :
-                break 
-            elif i <= 0 :
+            if i <= 0 :
                 cnt += 1
                 progresses.pop(0)
-                continue 
-            elif cnt >= 1 and i > 0 :
-                answer.append(cnt)
-                break 
-    answer.append(cnt)
+                continue
+            else :
+                break
+        if cnt >= 1 :
+            answer.append(cnt)
+            cnt = 0
                 
     return answer
+
