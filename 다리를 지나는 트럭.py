48c7d63bc20bd3334618ba3f3cdaa7e75ec09aeb
Sujin Jang <susan3722@snu.ac.kr>
2022-02-09 14:36:36 +0900

    Revert "풀이 2"
    This reverts commit 58700b7f91e62d4e941b0b83cc8740de93c2564d.

--- 다리를 지나는 트럭.py
+++ 다리를 지나는 트럭.py
@@ -1,22 +1,23 @@
 def solution(bridge_length, weight, truck_weights):
     
     from collections import deque
-    sum_truck = 0
-    bridge = deque([0] * bridge_length)
-    after_bridge = deque()
+    answer = 0
+    bridge = [0] * bridge_length
+    after_bridge = []
     truck_weights = deque(truck_weights)
   
-    while bridge :  
-        finish_truck = bridge.popleft()
-        sum_truck -= finish_truck
-        after_trucks.append(finish_truck)
-        if truck_weights :     
-            if (sum_truck + truck_weights[0]) <= weight : 
-                pop_truck = truck_weights.popleft()
-                bridge.append(pop_truck)
-                sum_truck += pop_truck
+    while truck_weights :  
+        try :  
+            while (sum(bridge[1:]) + truck_weights[0]) <= weight : 
+                bridge.append(truck_weights.popleft())
+                after_bridge.append(bridge[0])
+                bridge.pop(0)
+                answer += 1
             else :
                 bridge.append(0)
+                after_bridge.append(bridge[0])
+                bridge.pop(0)
+                answer += 1
 
-    return len(after_trucks)
-
+        except : 
+            return answer + len(bridge)
