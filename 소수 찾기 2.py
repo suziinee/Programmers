5c2dbab26f8bc2f200d23e8b8950ffe8647f6e7f
Sujin Jang <susan3722@snu.ac.kr>
2022-02-15 10:16:06 +0900

    풀이 2
    - set의 or 연산 이용해서 a에 특정 set을 더함 (set은 더하기 연산이 없음)
        - numbers는 string이므로 list로 만들어서 특정 개수의 순열을 돌림
        - permutation된 결과는 tuple이므로 그걸 join해서 int로 만들고 a에 넣음
    - 0과 1을 제외할때도 set 연산 사용
        - 이때 set(range())로 만들어서 set끼리 빼고 더해줘야함
    - 마지막으로 for문을 돌리기 위해 set을 list로 만들어서 소수 판별을 해줌

--- 소수 찾기 2.py
+++ 소수 찾기 2.py
@@ -1,18 +1,10 @@
 def solution(numbers):
     from itertools import permutations
-    nums = []
-    int_nums = []
+    a = set()
     primes = []
-    
-    for i in range(1, len(numbers) + 1) :
-        perms = list(permutations(numbers, i))
-        for perm in perms : 
-            string = ''
-            for num in perm :
-                string += str(num)
-            nums.append(string)
-    
-    int_nums = list(set([int(i) for i in nums]))
+
+    for i in range(len(numbers)) :
+        a |= set(map(int, map("".join, permutations(list(numbers), i + 1))))
     
     def prime_bool(n) :
         for i in range(2, n) :
@@ -20,8 +12,10 @@ def solution(numbers):
                 return False
         return True
     
-    for i in int_nums :
-        if i != 0 and i != 1 and prime_bool(i) == True :
+    a -= set(range(0, 2))
+    a = list(a)
+    for i in a :
+        if prime_bool(i) == True :
             primes.append(i)
     
     return len(primes)
