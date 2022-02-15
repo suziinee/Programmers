f0f06db226790f93840494fcf97fd8e5af02bb11
Sujin Jang <susan3722@snu.ac.kr>
2022-02-15 11:52:29 +0900

    풀이 2
    - 1만개는 시간초과가 되지 않으므로 학생들 정답 목록을 1만개 리스트로 만듦
    - 학생 index를 key, 맞춘 정답수를 value로 가진 ans 딕셔너리를 만들고 for문을 한번 돌림
    - ans 딕셔너리의 max_value 값과 같은 value의 key를 리턴함

--- 모의고사.py
+++ 모의고사.py
@@ -1,24 +1,21 @@
 def solution(answers):
-    s1 = [1,2,3,4,5]
-    cnt1 = 0
-    s2 = [2,1,2,3,2,4,2,5]
-    cnt2 = 0
-    s3 = [3,3,1,1,2,2,4,4,5,5]
-    cnt3 = 0
+    s1 = [1, 2, 3, 4, 5] * 2000
+    s2 = [2,1,2,3,2,4,2,5] * 1250
+    s3 = [3,3,1,1,2,2,4,4,5,5] * 1000
+    ans = {1 : 0, 2 : 0, 3 : 0}
     
     for i in range(len(answers)) :
         if answers[i] == s1[i%5] : 
-            cnt1 += 1
+            ans[1] += 1
         if answers[i] == s2[i%8] :
-            cnt2 += 1
+            ans[2] += 1
         if answers[i] == s3[i%10] :
-            cnt3 += 1
+            ans[3] += 1
     
-    d = {1 : cnt1, 2 : cnt2, 3 : cnt3}
-    ans = []
-    max_val = max(d.values())
-    for k, v in d.items() :
-        if v == max_val :
-            ans.append(k)
+    res = []
+    max_val = max(ans.values())
+    for i in range(1, 4) :
+        if ans[i] == max_val :
+            res.append(i)
             
-    return ans
+    return res
