86b50144ce22abdee2508655dae26c6811c8ff69
Sujin Jang <susan3722@snu.ac.kr>
2022-02-16 12:59:06 +0900

    풀이 2
    - 전체 넓이를 이용해서 간소화
        - 전체 넓이의 약수에 대한 for문을 돌리면서 약수마다 yellow에 부합하는지 확인
    - 마지막 리턴시 가로, 세로 구분 없이 sorted(reverse = True)를 사용해주면 내림차순으로 리턴됨

--- 카펫.py
+++ 카펫.py
@@ -1,19 +1,11 @@
-def solution(brown, yellow):
-    
-    def division(n) : # n의 약수 list를 구하는 함수
-        l = []
-        for i in range(1, n + 1) :
-            if n % i == 0 :
-                l.append(n // i)
-        return sorted(l)
-    
-    y_list = division(yellow)
-    if len(y_list) % 2 == 0 :
-        y_list = y_list[:len(y_list) // 2]
-    else :
-        y_list = y_list[:len(y_list) // 2 + 1]
-    
-    for i in y_list : # i는 세로 길이
-        j = int(yellow / i)
-        if (i + 2) * (j + 2) - yellow == brown :
-            return [j + 2, i + 2]
+def solution(brown, yellow) :
+
+    nm = brown + yellow
+
+    for n in range(1, nm + 1) :
+        if nm % n != 0 :
+            continue
+        else :
+            m = int(nm // n)
+            if (n - 2) * (m - 2) == yellow :
+                return sorted([n, m], reverse = True)
