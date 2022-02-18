ec655a9651951b75056807077ae70176658d77e8
Sujin Jang <susan3722@snu.ac.kr>
2022-02-18 21:08:54 +0900

    풀이 1
    - 첫번째 숫자를 key로 하고 원래 숫자를 [value]로 한 딕셔너리 생성
    - value의 길이가 1개라면 그대로 두고 여러개라면 permutation을 사용해서 안의 숫자들을 조합한 후 가장 큰 숫자를 value 값으로 함
    - sorted된 key list를 돌면서 ans에 value 값 string 형태로 더함
    - permutations 때문에 효율성 통과 못함

---
+++ 가장 큰 수.py
@@ -0,0 +1,24 @@
+def solution(numbers):
+   
+    d = {int(x[0]) : [] for x in list(map(str, numbers))} 
+    
+    for i in list(map(str, numbers)) :
+        d[int(i[0])].append(int(i))
+    
+    from itertools import permutations
+    
+    for key, value in d.items() :
+        if len(value) != 1 :
+            res = []
+            perms = list(permutations(value, len(value)))
+            for perm in perms :
+                res.append(int(''.join(list(map(str, perm)))))
+            d[key] = max(res)
+        else :
+            d[key] = value[0]
+            
+    ans = ''
+    for key in sorted(d.keys(), reverse = True) :
+        ans += str(d[key])
+        
+    return ans
