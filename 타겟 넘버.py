14e05d3cdfc8eb9055d15137bfa4c0c0f8827f4b
Sujin Jang <susan3722@snu.ac.kr>
2022-02-23 11:18:53 +0900

    Modified
    - numbers에 list comprehension을 통해 양수, 음수 추가
    - c = [0, 1] 변수 없앰 / len(numbers)는 효율성을 위해 변수로 둠
    - res 계산 시 list comprehension, sum 사용

--- 타겟 넘버.py
+++ 타겟 넘버.py
@@ -1,18 +1,14 @@
 def solution(numbers, target):
     
-    for i in range(len(numbers)) :
-        numbers[i] = [numbers[i], -numbers[i]]
+    numbers = [[x, -x] for x in numbers]
     
-    c = [0, 1]
     n = len(numbers)
     from itertools import product
-    prods = list(product(c, repeat = n))
+    prods = list(product([0, 1], repeat = n))
     
     ans = 0
     for prod in prods :
-        res = 0
-        for i in range(n) :
-            res += numbers[i][prod[i]]
+        res = sum([numbers[i][prod[i]] for i in range(n)])
         if res == target :
             ans += 1
         
