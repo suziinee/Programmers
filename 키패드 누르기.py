f2a30405fec0fd997cf63f6db14f6dbf7aeb9d5e
Sujin Jang <susan3722@snu.ac.kr>
2022-02-28 11:26:42 +0900

    Solution
    - 왼손, 오른손이 고정되어 있는 숫자를 제외하고 나머지 숫자 2, 5, 8, 0은 유클리드 거리를 이용해서 계산 후 왼손, 오른손 배정
    - 상하좌우만 이동할 수 있으므로 유클리드 거리를 이용하면 안됨 -> 실패
    - 파이썬 제곱연산은 ^가 아니라 **임을 기억하기

---
+++ 키패드 누르기.py
@@ -0,0 +1,41 @@
+def solution(numbers, hand) :
+
+    res = ""
+
+    l = (3, 0)
+    r = (3, 2)
+
+    d = {1:(0, 0), 2:(0, 1), 3:(0, 2), 4:(1, 0), 5:(1, 1), 6:(1, 2), 
+    7:(2, 0), 8:(2, 1), 9:(2, 2), 0:(3, 1)}
+
+    for num in numbers :
+        if num in [1, 4, 7] :
+            res += "L"
+            l = d[num]
+        elif num in [3, 6, 9] :
+            res += "R"
+            r = d[num]
+        else :
+            x = d[num][0]
+            y = d[num][1]
+
+            l_len = (l[0] - x)**2 + (l[1] - y)**2
+            r_len = (r[0] - x)**2 + (r[1] - y)**2
+
+            if l_len > r_len :
+                res += "R"
+                r = d[num]
+            elif l_len < r_len :
+                res += "L"
+                l = d[num]
+            else :
+                if hand == "right" :
+                    res += "R"
+                    r = d[num]
+                else :
+                    res += "L"
+                    l = d[num]
+    
+    return res
+
+
