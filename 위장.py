5686480425fd5d23d61f050eb17b176270b6f13b
Sujin Jang <susan3722@snu.ac.kr>
2022-02-28 17:15:23 +0900

    Solution 2
    - 하의 3개, 상의 2개가 있을 때 :
        (하의 3개 + 안입는 경우) * (상의 2개 + 안입는 경우) = 4 * 3 = 12
        모두 안입는 경우 1가지를 뺀 12 - 1 = 11가지
    - 수학식을 이용해서 1번 case 시간초과 해결

--- 위장.py
+++ 위장.py
@@ -6,18 +6,9 @@ def solution(clothes) :
     for c in clothes :
         d[c[1]] += 1
     
-    combs = set()
-    from itertools import combinations
-    for i in range(1, len(d) + 1) :
-        combs |= set(combinations(d.keys(), i))
+    ans = 1
+    for value in d.values() :
+        ans *= (value + 1)
     
-    combs = list(combs)
-    ans = 0
-    for c in combs :
-        mul = 1
-        for i in c :
-            mul *= d[i]
-        ans += mul
-        
-    return ans
+    return ans - 1
 
