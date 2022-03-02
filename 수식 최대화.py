0024fe5346c4236f17de7210135c3748aa8e6239
Sujin Jang <susan3722@snu.ac.kr>
2022-03-02 16:21:02 +0900

    Solution
    - 숫자와 기호를 분리해서 각각 순서대로 nums, ex 리스트에 담음
        - isdigit 메소드 사용
    - 최대 6가지 경우에 대해서 for문을 돌림
    - 복제된 기호 리스트를 돌면서 해당되는 우선순위의 기호가 아닐 경우,
        - 첫번째 숫자가 담겨진 stack_num에 숫자 추가 / 기호는 stack_ex에 추가
        - 우선순위 기호가 맞을 경우 eval을 이용해서 stack_num의 마지막 숫자 두개를 계산하고 다시 stack_num의 마지막에 append / 복제된 기호 리스트에서 계산 완료된 기호는 remove
    - 3개의 연산자에 대해서 모두 돌려줌
        - 연산자가 바뀔 때마다 copy_nums, copy_ex는 업데이트 / stack_nums, stack_ex는 초기화
    - 연산자 3개에 대해 모두 돌리고 나면 결과를 절댓값으로 ans 리스트에 추가
    - max(ans) 리턴

---
+++ 수식 최대화.py
@@ -0,0 +1,68 @@
+def solution(expression) :
+
+    # 쪼개기
+    ex = []
+    nums = []
+    res = ''
+    for e in expression :
+        if e.isdigit() :
+            res += e
+        else :
+            nums.append(int(res))
+            ex.append(e)
+            res = ''
+    nums.append(int(res))
+
+
+    combs = [[0, 1, 2], [0, 2, 1], [1, 0, 2], [1, 2, 0], [2, 1, 0], [2, 0, 1]]
+    symbol = ['*', '-', '+']
+    ans = []
+
+    from collections import deque
+    for c in combs :
+        res = 0
+        copy_nums = deque(nums.copy())
+        copy_ex = ex.copy()
+        stack_nums = [copy_nums.popleft()]
+        stack_ex = []
+
+        for e in copy_ex[:] : 
+            stack_nums.append(copy_nums.popleft())
+            if e == symbol[c[0]] :
+                res = eval(str(stack_nums.pop(-2)) + e + str(stack_nums.pop(-1)))
+                stack_nums.append(res)
+                copy_ex.remove(e)
+            else : 
+                stack_ex.append(copy_ex.pop(0))
+        
+        copy_nums = deque(stack_nums)
+        copy_ex = stack_ex
+        stack_nums = [copy_nums.popleft()]
+        stack_ex = []
+        for e in copy_ex[:] : 
+            stack_nums.append(copy_nums.popleft())
+            if e == symbol[c[1]] :
+                res = eval(str(stack_nums.pop(-2)) + e + str(stack_nums.pop(-1)))
+                stack_nums.append(res)
+                copy_ex.remove(e)
+            else :
+                stack_ex.append(copy_ex.pop(0))
+        
+        copy_nums = deque(stack_nums)
+        copy_ex = stack_ex
+        stack_nums = [copy_nums.popleft()]
+        stack_ex = []
+        for e in copy_ex[:] : #"-"
+            stack_nums.append(copy_nums.popleft())
+            if e == symbol[c[2]] :
+                res = eval(str(stack_nums.pop(-2)) + e + str(stack_nums.pop(-1)))
+                stack_nums.append(res)
+                copy_ex.remove(e)
+            else :
+                stack_ex.append(copy_ex.pop(0))
+
+        ans.append(abs(stack_nums[0]))
+
+    return max(ans)
+
+
