ce4cddac04dcea55cc9a6d1b1aaf4ebe2edca8e0
Sujin Jang <susan3722@snu.ac.kr>
2022-03-03 10:10:59 +0900

    Modified
    - 문자열 splicing 부분을 간소화

--- 튜플.py
+++ 튜플.py
@@ -1,14 +1,7 @@
 def solution(s):
     
-    s = s[1:-1]
-    s = s.split('}')
-    s = [i for i in s if i]
-    
-    for i in range(len(s)) :
-        if i == 0 :
-            s[i] = s[i][1:]
-        else :
-            s[i] = s[i][2:]
+    s = s[2:-2]
+    s = s.split('},{')
     
     s = [list(map(int, i.split(','))) for i in s]
     s = sorted(s, key = lambda x : len(x))
