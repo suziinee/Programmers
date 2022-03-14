6d09198af477307722573001624d312c7848ca0c
Sujin Jang <susan3722@snu.ac.kr>
2022-03-14 20:46:25 +0900

    Solution 3
    - phone_book을 길이로 sort한 후 값으로 sort함
        - 길이 sort : [12, 88, 123, 567, 1235]
        - 값 sort : [12, 123, 1235, 567, 88]
    - 이후 find 함수를 이용해 앞뒤 인덱스끼리만 비교해 주면 됨
    - 효율성 모두 통과

--- 전화번호 목록.py
+++ 전화번호 목록.py
@@ -1,18 +1,18 @@
 def solution(phone_book) :
 
+    phone_book.sort(key = lambda x : len(x))
     phone_book.sort()
 
-    from collections import deque
-    pb = deque(phone_book)
-
-    hash = {p : 0 for p in phone_book}
-
-    while pb :
-        now = pb.popleft()
-        for key in hash.keys() :
-            if key.find(now) == 0 and key != now : 
-                return False
+    for i in range(len(phone_book) - 1) :
+        if phone_book[i + 1].find(phone_book[i]) == 0 :
+            return False
+    
     return True
+    
+    
+
+    
+
     
 
 
