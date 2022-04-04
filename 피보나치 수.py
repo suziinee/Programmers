1acc0a1ccd45c50f9f46675c93370ebff663c73b
Sujin Jang <susan3722@snu.ac.kr>
2022-04-04 14:02:03 +0900

    Solution 2
    - dynamic programming top down 방식
        - memorization을 사용해서 O(N)

--- 피보나치 수.py
+++ 피보나치 수.py
@@ -1,14 +1,13 @@
 def solution(n):
     
-    import sys
-    sys.setrecursionlimit(10**7)
+    d = [0] * 100
+
+    def fibo(x) :
+        if x == 1 or x == 2 :
+            d[x] = 1
+        if d[x] != 0 :
+            return d[x]
+        d[x] = d[x-1] + d[x-2]
+        return d[x]
     
-    d = [0] * 100001
-    
-    for i in range(n + 1) :
-        if i == 1 or i == 2 :
-            d[i] = 1
-        else :
-            d[i] = d[i-1] + d[i-2]
-    
-    return d[n] % 1234567
+    fibo(n)
