4107f07847525b72fdc3992b6dba14e1b3ed478d
Sujin Jang <susan3722@snu.ac.kr>
2022-04-13 15:22:27 +0900

    Solution 2
    - move라는 변수를 이용해서 거리의 최적을 계속 초기화
    - move의 초기 값은 왼쪽-오른쪽 방향으로 갔을때 거리로 설정
    - A가 아닌 매 순간에 대해서
        - ans에 A로 만들어주는 횟수를 더하고
        - A가 아닌 다음 요소에 대해서 왼-오 방향과 꺾어서 오는 방향 중에 작은 값을 구해서 move를 갱신
    - 반복문을 모두 돌았을 때 ans에 문자 변환 횟수는 모두 더해져 있으므로 거리인 move만 더하면 됨

--- Greedy/조이스틱.py
+++ Greedy/조이스틱.py
@@ -6,19 +6,25 @@ def solution(name) :
     def toA(alphabet) :
         return min(ord(alphabet) - ord("A"), ord("Z") - ord(alphabet) + 1)
 
-    forward = count[1:]
-    backward = count[1:][::-1]
-
-    if forward[-1] == 0 :
-        f = sum(forward) + n - 1 - 1
-    else :
-        f = sum(forward) + n - 1
-
-    if backward[-1] == 0 :
-        b = sum(backward) + n - 1 - 1
-    else :
-        b = sum(backward) + n - 1
+    # 첫번째 move는 온전히 왼-오로 가는 거리
+    for i in range(n) :
+        if name[i] != 'A' :
+            move = i 
     
-    return ans + min(f, b)
+    for i in range(n) :
+        if name[i] != 'A' :
+            ans += toA(name[i])
+            next_i = i + 1
+
+            # A가 아닌 다음 요소가 나올때까지 next_i 초기화
+            while next_i < n and name[next_i] == 'A' :
+                next_i += 1
+            
+            # 매 순간 왼-오 경우와 오-꺾 경우를 생각해주기
+            move = min(move, i * 2 + (n - next_i))
+            
+    ans += move
+    return ans
+
         
 
