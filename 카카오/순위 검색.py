bacf77efc1e5ce654a95a8b82a5d279f940cf9b1
Sujin Jang <susan3722@snu.ac.kr>
2022-04-19 15:20:48 +0900

    Soluion 3
    - info의 모든 조합에 대해서 info_dict에 key는 조합, value는 점수 리스트로 만들어놓음
    - value는 오름차순으로 sort
    - 각 query에 대해 조건에 만족하는 info_dict key에서 query의 score보다 큰 점수에 대해 bisect_left를 찾아서 len(value) - index를 ans에 추가
        - 점수 계산에서 sorting을 통해 효율성을 높임
    - 효율성 통과

--- 카카오/순위 검색.py
+++ 카카오/순위 검색.py
@@ -1,28 +1,35 @@
 def solution(info, query) :
 
-    info_dict = {}
-    ans = []
+    from collections import defaultdict
+    from itertools import product
+    from bisect import bisect_left
 
-    for i, v in enumerate(info) :
-        info_dict[i] = v.split()
-    
-    query = [[x for x in q.split() if x not in ['and', '-']] for q in query]
+    info_dict = defaultdict(list)
+    ans = []
+    binarys = list(product(True, False), repeat = 4)
+
+    for i in info :
+        i = i.split()
+        score = int(i.pop())
+        for b in binarys :
+            key = ""
+            for k in range(4) :
+                if b[k] :
+                    key += i[k]
+                    key += "/"
+                else :
+                    key += "-"
+                    key += "/"
+            info_dict[key[:-1]].append(score)
 
     # 점수 정렬
-    info_dict = sorted(info_dict.items(), key = lambda x : int(x[1][-1]))
+    info_dict = {key : sorted(value) for key, value in info_dict.items()}
 
-    # 점수만 따로 빼기
-    scores = [int(x[1][-1]) for x in info_dict]
-
-    from bisect import bisect_left
-    
+    query = [x.split() for x in query]
     for q in query :
         score = int(q.pop())
-        idx = bisect_left(scores, score)
-        cnt = 0
-        for x in info_dict[idx:] :
-            if set(q) - set(x[1]) == set() :
-                cnt += 1
-        ans.append(cnt)
-
+        key = "".join(q).replace("and", "/")
+        x = bisect_left(info_dict[key], score)
+        ans.append(len(info_dict[key] - x))
+    
     return ans
